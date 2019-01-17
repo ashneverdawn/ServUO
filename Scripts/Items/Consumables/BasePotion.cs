8d10a6df505ce00c5a2a3f004eaebc4d5d139a7a
Hugo <ashneverdawn@gmail.com>
2019-01-17 10:53:02 -0500

    Use base skill for enhance potions alchemy bonus

--- Scripts/Items/Consumables/BasePotion.cs
+++ Scripts/Items/Consumables/BasePotion.cs
@@ -231,7 +231,7 @@ namespace Server.Items
         public static int EnhancePotions(Mobile m)
         {
             int EP = AosAttributes.GetValue(m, AosAttribute.EnhancePotions);
-            int skillBonus = m.Skills.Alchemy.Fixed / 330 * 10;
+            int skillBonus = m.Skills.Alchemy.BaseFixedPoint / 330 * 10;
 
             if (Core.ML && EP > 50 && m.IsPlayer())
                 EP = 50;
