0740172fcfa450d445ea448914703697af06ad25
Hugo <ashneverdawn@gmail.com>
2019-02-01 00:10:19 -0500

    Fix WoD (wasnt direct damage)

--- Scripts/Spells/Spellweaving/WordOfDeath.cs
+++ Scripts/Spells/Spellweaving/WordOfDeath.cs
@@ -76,10 +76,7 @@ namespace Server.Spells.Spellweaving
                 damage *= damageBonus + 100;
                 damage /= 100;
 
-                int[] types = new int[4];
-                types[Utility.Random(types.Length)] = 100;
-
-                SpellHelper.Damage(this, m, damage, 0, types[0], types[1], types[2], types[3]);	//Chaos damage.  Random elemental damage
+                SpellHelper.Damage(this, m, damage, 0, 0, 0, 0, 0, 0, 100);	//Direct damage
             }
 
             this.FinishSequence();
